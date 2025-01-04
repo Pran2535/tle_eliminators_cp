2dc6d668e790d1e8d7930a932f49b58359f55bf5
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2025-01-04 19:37:56 +0530

    third commit

---
+++ decodedsa/threediv.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Enter a number: ";
+    cin >> a;
+    for (int i = 1; i <= a; i++)
+    {
+        if (i % 3 == 0)
+        {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
