a6f313c34d7655038eaa52e1e713a4ab43713d84
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2025-01-11 19:31:51 +0530

    stl completed baki dhere dhere samajh aan jaiega

---
+++ decodedsa/stl/vectors.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    auto it = "hello kaise ho";
+    cout << it << endl;
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(40);
+    v.push_back(50);
+    cout << v.size() << endl;
+    // here we are learning how to declare vector and use it and so we are declaring vectors with the help of the
+    // context vector<int> v; by this line we use to declare the vector of type integer and integer it helps us to store the data in the form of dynamic array
+    // are matlab ye hai ki ye automatically apna size increase kar leta hai
+    return 0;
+}
