2e27b46ba06733159040a9c97bf5a50c607bae6c
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2025-01-14 19:55:10 +0530

    vectors understood

---
+++ .history/decodedsa/arrays/vectors_20250114195453.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> mm;
+    for (int i = 0; i < 5; i++)
+    {
+        mm.push_back(i);
+    }
+    for (int j = 0; j < mm.size(); j++)
+    {
+        cout << mm[j] << " ";
+    }
+    return 0;
+}
