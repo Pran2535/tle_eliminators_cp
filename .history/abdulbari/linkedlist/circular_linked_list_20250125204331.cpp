53f7616dd94de8a2e3b1b155e2249743c03d6276
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2025-01-25 20:45:57 +0530

    circular linked list implemented

---
+++ .history/abdulbari/linkedlist/circular_linked_list_20250125204331.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node *next;
+};
+void print(node *p)
+{
+    node *temp = p;
+    while (temp->next != p)
+    {
+        cout << temp->data << ",";
+    }
+}
+int main()
+{
+    node *first = new node;
+    node *second = new node;
+    node *third = new node;
+    node *fourth = new node;
+    first->data = 1;
+    first->next = second;
+    second->data = 2;
+    second->next = third;
+    third->data = 3;
+    third->next = fourth;
+    fourth->data = 4;
+    fourth->next = first;
+    print(first);
+    return 0;
+}
