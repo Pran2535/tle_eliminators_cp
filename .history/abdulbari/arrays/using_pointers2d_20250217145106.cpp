af03ab50765f979311b242daf538e3c527a4e2fd
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2025-02-17 14:51:26 +0530

    using pointers done

---
+++ .history/abdulbari/arrays/using_pointers2d_20250217145106.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int *a[3];
+    int l = 0;
+    a[0] = new int[4];
+    a[1] = new int[4];
+    a[2] = new int[4];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            a[i][j] = l++;
+        }
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << a[i][j] << ",";
+        }
+    }
+
+    return 0;
+}
