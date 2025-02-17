2a7d414e48de0334548dd931f013932026e5b05c
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2025-02-17 19:57:34 +0530

    binary search completed

---
+++ .history/abdulbari/adts/binary_search_20250217195708.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int binary_search(int arr[], int target, int low, int high)
+{
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    int arr[] = {1, 2, 11, 13, 18, 21};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 13;
+    cout << binary_search(arr, target, 0, n - 1);
+    return 0;
+}
