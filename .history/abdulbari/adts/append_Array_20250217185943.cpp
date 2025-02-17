affc57e36f5cbcbaafa829157db2df059c873e01
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2025-02-17 19:03:11 +0530

    insert function completed

---
+++ .history/abdulbari/adts/append_Array_20250217185943.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+struct Array
+{
+    int A[20];
+    int size;
+    int length;
+};
+void display(struct Array arr)
+{
+    int i;
+    cout << "elements are ";
+    for (i = 0; i < arr.length; i++)
+    {
+        cout << arr.A[i] << " ";
+    }
+}
+int main()
+{
+    struct Array arr = {{2, 3, 4, 5, 6}, 20, 5};
+    display(arr);
+    return 0;
+}
---
+++ .history/abdulbari/adts/append_Array_20250217190248.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+struct Array
+{
+    int A[20];
+    int size;
+    int length;
+};
+void display(struct Array &arr)
+{
+    int i;
+    cout << "elements are ";
+    for (i = 0; i < arr.length; i++)
+    {
+        cout << arr.A[i] << " ";
+    }
+}
+void insert(struct Array &arr, int index, int data)
+{
+    for (int i = arr.length; i > index; i--)
+    {
+        arr.A[i] = arr.A[i - 1];
+    }
+    arr.A[index] = data;
+    arr.length++;
+}
+int main()
+{
+    struct Array arr = {{2, 3, 4, 5, 6}, 20, 5};
+    display(arr);
+    cout << endl;
+    insert(arr, 2, 10);
+    display(arr);
+    return 0;
+}
