a18f69257a4062f9bdb8fa5d49808dc5299219f1
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2025-02-17 20:11:36 +0530

    binary search using recursion done

---
+++ .history/abdulbari/adts/binary_search_using_recursion_20250217201112.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int search(int arr[], int low, int high, int target)
+{
+    if (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == target)
+        {
+            return mid;
+        }
+        if (arr[mid] > target)
+        {
+            return search(arr, low, mid - 1, target);
+        }
+        return search(arr, mid + 1, high, target);
+    }
+    return -1;
+}
+int main()
+{
+    int arr[] = {1, 2, 5, 10, 15, 25};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << search(arr, 0, n - 1, 15);
+    return 0;
+}
