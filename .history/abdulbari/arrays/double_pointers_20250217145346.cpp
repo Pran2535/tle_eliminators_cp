4576e717de93c3df94645408ea24318c9ce85007
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2025-02-17 14:54:09 +0530

    using 2 pointers done here

---
+++ .history/abdulbari/arrays/double_pointers_20250217145346.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int **p;
+    p = new int *[3];
+    p[0] = new int[4];
+    p[1] = new int[4];
+    p[2] = new int[4];
+    int l = 0;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            p[i][j] = l++;
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << p[i][j] << ",";
+        }
+
+        return 0;
+    }
