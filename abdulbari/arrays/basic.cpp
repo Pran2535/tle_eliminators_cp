f808750d0a08dd408fb2e09c2f1e010cc9de92b6
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2025-02-17 11:54:35 +0530

    garbage value initialization done

---
+++ abdulbari/arrays/basic.cpp
@@ -0,0 +1,17 @@
+// basic data type
+
+// arrays are the collections of similar types of data arranged in such a way
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 10;
+    int a[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cout << a[i] << ",";
+    }
+    return 0;
+}
