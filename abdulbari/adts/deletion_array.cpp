67904c7649023a9ccaf25fc6e8d731848799290d
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2025-02-17 19:21:55 +0530

    deletion done

---
+++ abdulbari/adts/deletion_array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void deletion(int index, int arr[], int size)
+{
+    if (index >= size)
+    {
+        return;
+    }
+    for (int i = index; i < size; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+    arr[size - 1] = 0;
+}
+void display(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int index;
+    cout << "Enter the index you want to delete: ";
+    cin >> index;
+    display(arr, size);
+    deletion(index, arr, size);
+    cout << endl;
+    display(arr, size - 1);
+
+    return 0;
+}
