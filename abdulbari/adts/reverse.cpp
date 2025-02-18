54ad0ea8bbbd37a90d590f0baeafd3c5d7bff432
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2025-02-18 12:05:13 +0530

    reverseal done

---
+++ abdulbari/adts/reverse.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+void reverse(int arr[], int size)
+{
+    int start = 0;
+    int end = size - 1;
+    while (start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    reverse(arr, sizeof(arr) / sizeof(arr[0]));
+    return 0;
+}
