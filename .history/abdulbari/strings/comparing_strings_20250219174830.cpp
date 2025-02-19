770bb072f8973311af514ee75e4d3606f9b1f36b
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2025-02-19 17:49:47 +0530

    comparing strings on cpp

---
+++ .history/abdulbari/strings/comparing_strings_20250219174830.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char ch[] = "Painter";
+    char bh[] = "Painting";
+    int i, j;
+    for (i = 0, j = 0; ch[i] != '\0' && bh[j] != '\0'; i++, j++)
+    {
+        if (ch[i] != bh[j])
+            ;
+        break;
+    }
+    if (ch[i] == bh[j])
+    {
+        cout << "equal";
+    }
+    else if (ch[i] < bh[j])
+    {
+        cout << "ch is smaller";
+    }
+    else
+    {
+        cout << "bh is smaller";
+    }
+    return 0;
+}
