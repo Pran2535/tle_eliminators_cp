fed2e51b04bbe2d983b5abb33b8d3474b4222385
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2025-02-19 11:08:04 +0530

    union of two arrays done

---
+++ abdulbari/adts/union_two_arrays.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+void union_two(int arr[], int brr[], int crr[], int m, int n)
+{
+    int total = m + n;
+    int k = 0;
+    for (int i = 0; i < m; i++)
+    {
+        crr[k++] = arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (brr[i] == arr[j])
+            {
+                break;
+            }
+            if (j == m - 1)
+            {
+                crr[k++] = brr[i];
+            }
+        }
+    }
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int brr[] = {4, 5, 6, 7, 8, 9};
+    int crr[11];
+    union_two(arr, brr, crr, 5, 6);
+    for (int i = 0; i < 11; i++)
+    {
+        cout << crr[i] << ",";
+    }
+    return 0;
+}
