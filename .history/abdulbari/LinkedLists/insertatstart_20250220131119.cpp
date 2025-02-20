06181cb4aa009331d993cf77823af092bf19e377
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2025-02-20 13:12:15 +0530

    insert at start done

---
+++ .history/abdulbari/LinkedLists/insertatstart_20250220131119.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node *next;
+};
+void createLinkedList(int arr[], int size, node *&head)
+{
+    for (int i = 0; i < size; i++)
+    {
+        node *newNode = new node();
+        newNode->data = arr[i];
+        newNode->next = NULL;
+        if (head == NULL)
+        {
+            head = newNode;
+        }
+        else
+        {
+            node *temp = head;
+            while (temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+}
+node *insertNode(int data, node *&head)
+{
+    node *p = new node();
+    p->data = data;
+    p->next = head;
+    head = p;
+    return head;
+}
+void print(node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    node *head = NULL;
+    createLinkedList(arr, 5, head);
+    // cout << largest(0, head);
+    // cout << search(head, 3);
+    head = insertNode(4, head);
+    print(head);
+    return 0;
+}
