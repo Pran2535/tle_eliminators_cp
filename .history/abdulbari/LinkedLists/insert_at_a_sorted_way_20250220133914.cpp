396fb23c1d7d6cad63edfbc5243d069ffd0381b0
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2025-02-20 13:41:38 +0530

    insertion in a sorted way done

---
+++ .history/abdulbari/LinkedLists/insert_at_a_sorted_way_20250220133914.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node *next;
+};
+void createLinkedList(int arr[], int size, node *&head)
+{
+    for (int i = 0; i < size; i++)
+    {
+        node *newNode = new node();
+        newNode->data = arr[i];
+        newNode->next = NULL;
+        if (head == NULL)
+        {
+            head = newNode;
+        }
+        else
+        {
+            node *temp = head;
+            while (temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+}
+node *insertNode(int data, node *&head)
+{
+    node *ptr1 = head;
+    node *ptr2 = head->next;
+    while ((ptr1->data < data) && (ptr2->data < data))
+    {
+        ptr1 = ptr1->next;
+        ptr2 = ptr2->next;
+    };
+    node *newNode = new node();
+    newNode->data = data;
+    ptr1->next = newNode;
+    newNode->next = ptr2;
+
+    return head;
+}
+void print(node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    node *head = NULL;
+    createLinkedList(arr, 5, head);
+    // cout << largest(0, head);
+    // cout << search(head, 3);
+    head = insertNode(4, head);
+    head = insertNode(6, head);
+    print(head);
+    return 0;
+}
---
+++ .history/abdulbari/LinkedLists/insert_at_a_sorted_way_20250220134031.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+};
+
+// Function to create a linked list from an array
+void createLinkedList(int arr[], int size, node *&head)
+{
+    head = NULL; // Ensure head is initially NULL
+    node *tail = NULL;
+
+    for (int i = 0; i < size; i++)
+    {
+        node *newNode = new node();
+        newNode->data = arr[i];
+        newNode->next = NULL;
+
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+}
+
+// Function to insert a node in sorted order
+node *insertNode(int data, node *&head)
+{
+    node *newNode = new node();
+    newNode->data = data;
+    newNode->next = NULL;
+
+    // If list is empty or data is smaller than head
+    if (head == NULL || head->data > data)
+    {
+        newNode->next = head;
+        head = newNode;
+        return head;
+    }
+
+    node *ptr1 = head;
+    node *ptr2 = head->next;
+
+    // Traverse until we find the right position
+    while (ptr2 != NULL && ptr2->data < data)
+    {
+        ptr1 = ptr1->next;
+        ptr2 = ptr2->next;
+    }
+
+    // Insert the new node between ptr1 and ptr2
+    ptr1->next = newNode;
+    newNode->next = ptr2;
+
+    return head;
+}
+
+// Function to print the linked list
+void print(node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+// Main function
+int main()
+{
+    int arr[] = {1, 2, 3, 5}; // Removed 4 to test insertion
+    node *head = NULL;
+
+    createLinkedList(arr, 4, head);
+
+    head = insertNode(4, head); // Insert 4 at correct position
+    head = insertNode(6, head); // Insert 6 at correct position
+
+    print(head); // Expected Output: 1 2 3 4 5 6
+
+    return 0;
+}
