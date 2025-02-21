2c5c62c97f2e0523488560c28a21329aac56539d
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2025-02-21 12:16:25 +0530

    insert at start done

---
+++ .history/abdulbari/LinkedLists/circular_linked_lists/insertion_in_list_20250221121312.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node *next;
+};
+node *insertatstart(node *head, int data)
+{
+    node *p = head;
+    p = p->next;
+    while (p != head)
+    {
+        p = p->next;
+    }
+
+    node *newNode = new node();
+    newNode->data = data;
+    newNode->next = head;
+    head = newNode;
+    // tail->next = newNode;
+    p->next = newNode;
+    head = newNode;
+    return head;
+}
+void createLinkedList(int arr[], int size, node *&head)
+{
+    head = NULL; // Ensure head is initially NULL
+    node *tail = NULL;
+
+    for (int i = 0; i < size; i++)
+    {
+        node *newNode = new node();
+        newNode->data = arr[i];
+        newNode->next = NULL;
+
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    tail->next = head;
+}
+void display(node *head)
+{
+    node *temp = head;
+    cout << temp->data << "->";
+    temp = temp->next;
+    while (temp != head)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+}
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    node *head = 0;
+    createLinkedList(arr, size, head);
+    insertatstart(head, 10);
+    display(head);
+    return 0;
+}
---
+++ .history/abdulbari/LinkedLists/circular_linked_lists/insertion_in_list_20250221121529.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+};
+
+// Inserts a new node at the beginning of a circular linked list
+node *insertatstart(node *head, int data)
+{
+    // Create a new node with the given data
+    node *newNode = new node();
+    newNode->data = data;
+
+    // If the list is empty, point newNode->next to itself and return it as head
+    if (head == NULL)
+    {
+        newNode->next = newNode;
+        return newNode;
+    }
+
+    // Find the last node (tail) whose next is head
+    node *p = head;
+    while (p->next != head)
+    {
+        p = p->next;
+    }
+
+    // Insert newNode before head:
+    newNode->next = head;
+    p->next = newNode;
+
+    // newNode becomes the new head
+    return newNode;
+}
+
+// Creates a circular linked list from an array
+void createLinkedList(int arr[], int size, node *&head)
+{
+    head = NULL; // Ensure head is initially NULL
+    node *tail = NULL;
+
+    for (int i = 0; i < size; i++)
+    {
+        node *newNode = new node();
+        newNode->data = arr[i];
+        newNode->next = NULL;
+
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    // Make it circular by linking tail to head
+    if (tail != NULL)
+        tail->next = head;
+}
+
+// Displays the circular linked list
+void display(node *head)
+{
+    if (head == NULL)
+        return;
+
+    node *temp = head;
+    do
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    } while (temp != head);
+    cout << "..." << endl; // Indicates circularity
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    node *head = NULL;
+
+    createLinkedList(arr, size, head);
+    head = insertatstart(head, 10);
+    display(head);
+
+    return 0;
+}
